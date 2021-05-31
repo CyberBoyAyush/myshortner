d5898a05f0ae52aad82686572a4ab9bc9599d436
AYU$H $HARMA <aayush240204@gmail.com>
2021-05-31 10:09:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+"""URL Shortener Hack"""
+import json
+import os
+import shutil
+
+
+def main():
+    """Main Function"""
+    html = '<html><head><meta http-equiv="refresh" content="0;url={url}" /></head></html>'
+
+    with open('links.json') as f:
+        links = json.load(f)
+
+    shutil.rmtree('dist', ignore_errors=True)
+    os.mkdir('dist')
+
+    with open('dist/CNAME', 'w') as f:
+        f.write('aysh.xyz')
+
+    for link in links:
+        html_document = html.format(url=link['url'])
+        file_path = f"dist/{link['name']}.html"
+
+        with open(file_path, 'w') as f:
+            f.write(html_document)
+
+
+if __name__ == "__main__":
+    main()
